7c36530b7baa68df7c8fee91eb0ec9ceed71ce3f
Austin Shindlecker <ashindlecker@gmail.com>
2015-04-30 04:43:48 -0400

    Youtube downloading, can successfully request and download a video

--- ContentDownloaderServer/app.js
+++ ContentDownloaderServer/app.js
@@ -1,1 +1,51 @@
-﻿console.log('Hello world');
+﻿var static = require("node-static");
+var fileServer = new static.Server("./media");
+var child_process = require("child_process");
+
+
+var httpServer = require("http").createServer(function (request, response) {
+    request.addListener('end', function () {
+        fileServer.serve(request, response);
+    }).resume();
+}).listen(8080);
+var socket = require("socket.io")(httpServer);
+
+var counter = 0;
+socket.on("connection", function (client) {
+    
+    client.emit("success", { message: "welcome" });
+
+    client.on('request', function (data) {
+        var url = data["url"];
+        var type = data["type"];
+        
+        console.log(data);
+        
+        var tempCount = counter++;
+        var dl = child_process.spawn("python", ["-m", "youtube_dl", "--no-playlist", url, "-o", "./media/tmp/" + tempCount + "/%(title)s.%(ext)s"]);
+
+        dl.stdout.on("data", function (data) {
+            data = data.toString();
+            console.log(data);
+            var percent = parseInt(data.substring(11, data.indexOf("%")));
+            if (isNaN(percent) == false) {
+                client.emit("progress", { pr: percent });
+            }
+
+            if (data.indexOf("Destination") > -1) {
+                var destination = data.substring(29, data.indexOf("\n"));
+                console.log(destination);
+                client.emit("link", { url: destination });
+            }
+        });
+        
+
+        if (type == "video") {
+
+        }
+
+    });
+});
+
+
+
---
+++ ContentDownloaderSite/js/contentclient.js
@@ -0,0 +1,36 @@
+﻿
+var contentclient = function()
+{
+    this.connected = false;
+    this.socket = null;
+};
+
+contentclient.prototype.onSuccess = function()
+{
+    console.log("connected");
+}
+
+contentclient.prototype.progress = function(data)
+{
+    console.log(data);
+}
+
+contentclient.prototype.downloadLink = function (data) {
+    console.log(data);
+}
+
+contentclient.prototype.connect = function(ip)
+{
+    this.socket = io(ip);
+    this.socket.on("success", this.onSuccess);
+    this.socket.on("progress", this.progress);
+    this.socket.on("link", this.downloadLink);
+    this.connected = true;
+}
+
+contentclient.prototype.startDownload = function(url, content_type)
+{
+    if (!this.connected) return false;
+    this.socket.emit("request", { url: url, type: content_type });
+
+}
